9b59a6b75ad5d13039e0d9302327d3d3ef91914f
Nicolaas <github@sunnysideup.co.nz>
2009-04-26 10:35:01 +0000

    moved _config.php file

--- googleanalyticsbasics/_config.php
+++ googleanalyticsbasics/_config.php
@@ -5,12 +5,11 @@
  * author: Nicolaas - modules [at] sunnysideup.co.nz
 **/
 
-//DataObject::add_extension('SiteTree', 'GoogleAnalytics');
-//GoogleAnalytics::$googleAnalyticsCode = ""; //e.g. UA-xxxx-y
-
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START googleAnalyticsbasics MODULE ----------------===================
+//DataObject::add_extension('SiteTree', 'GoogleAnalytics');
+//GoogleAnalytics::$googleAnalyticsCode = ""; //e.g. UA-xxxx-y
 //===================---------------- END googleAnalyticsbasics MODULE ----------------===================
 
 
