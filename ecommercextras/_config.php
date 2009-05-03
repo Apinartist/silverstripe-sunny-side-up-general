64f1b3a5e73a2877691d10f7472e554a89f227bd
Nicolaas <github@sunnysideup.co.nz>
2009-05-03 01:18:53 +0000

    added LICENSE and _config material

--- ecommercextras/_config.php
+++ ecommercextras/_config.php
@@ -1,4 +1,16 @@
 <?php
 
+
+/**
+* developed by www.sunnysideup.co.nz
+* author: Nicolaas modules [at] sunnysideup.co.nz
+*
+**/
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START geobrowser MODULE ----------------===================
+//PhotographicProductGroup::set_can_create(true);
+//PhotographicProduct::set_can_create(true);
+//===================---------------- END geobrowser MODULE ----------------===================
+
 //
-PhotographicProductGroup::set_can_create(true);
