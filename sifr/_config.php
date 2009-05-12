7fdc16ec09edad9e498a0be62d5a1a9ec055c04a
Nicolaas <github@sunnysideup.co.nz>
2009-05-12 23:09:35 +0000

    overhaul to version 3

--- sifr/_config.php
+++ sifr/_config.php
@@ -9,13 +9,9 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START sifr MODULE ----------------===================
-/*
-//you can have multiple replacements listed here
-Sifr::replace_element($named = "porcelain", $element = "h1", $font = "porcelain");
-Sifr::set_colours_and_case($named = "porcelain", $sColor = "#000000", $sLinkColor = "#555555", $bBgColor = "#FFFFFF", $sHoverColor = "#999999", $sWmode = "transparent", $sCase = "");
-Sifr::set_padding($named = "porcelain", $nPaddingTop = 0, $nPaddingBottom = 0, $nPaddingRight = 0, $nPaddingLeft = 0);
-Sifr::set_position($named = "porcelain", $textalign = "left", $offsetLeft = 0, $offsetTop = 0);
-*/
+//Sifr::set_js_custom_file_location("mysite/javascript/sifr.js");
+//Sifr::load_sifr();
+//Sifr::disable();
 //===================---------------- END sifr MODULE ----------------===================
 
 
--- sifr/code/Sifr.php
+++ sifr/code/Sifr.php
@@ -14,153 +14,32 @@
 
 class Sifr extends Object {
 
-	static $hide_browser_text = true;
+	protected static $is_disabled = false;
 
-	static $b_fix_frag_id_bug = true;
+	protected static $js_custom_file_location = "mysite/javascript/sifr.js";
 
-	static $is_disabled = false;
-
-	static $fonts = array();
-
-	static $js = "";
-
-	public function loadSifr() {
-		$js = "
-			if(typeof sIFR == 'function'){";
-		if(self::$is_disabled == true) {
-			$js .= 'sIFR.bIsDisabled = true;';
-		}
-		else {
-			foreach(self::$fonts as $font) {
-				$js .= '
-					sIFR.replaceElement(named({';
-				$i = 0;
-				foreach($font as $key=>$attribute) {
-					if($attribute) {
-						if(is_array($attribute)) {
-							$js .= ', sFlashVars: "';
-							$j = 0;
-							foreach($attribute as $name => $value) {
-								if($value) {
-									if($j) {
-										$js .= '&';
-									}
-									$js .= $name.'='.$value;
-									$j++;
-								}
-							}
-							$js .= '"';
-						}
-						else {
-							if($i) {
-							 $js .= ', ';
-							}
-							$js .= $key.':"' . $attribute.'"';
-						}
-						$i++;
-					}
-				}
-				$js .= "}));\n";
-			}
-		}
-		$js .= "sIFR.setup();\n";
-		$js .= "}";
-		Requirements::themedCSS("sIFR-screen.css", 'screen, projection');
-		Requirements::themedCSS("sIFR-print.css", 'print');
-		Requirements::javascript("sifr/javascript/sifr.js");
-		Requirements::javascript("sifr/javascript/sifr-addons.js");
-		Requirements::customScript($js);
-		self::$fonts = array();
-	}
-	/*
-
-
-	*/
-	static function replace_element($named = "sifr", $element = "", $font = "") {
-		$theme = SSViewer::current_theme();
-		if(!$theme || 1 == 1) {
-			$theme = 'sifr';
-		}
-		else {
-			$theme = 'themes/'.$theme;
-		}
-		$file = $theme.'/fonts/'.$font.'.swf';
-		if(!file_exists("../".$file)) {
-			return die("can not find: ".$file);
-		}
-		self::$fonts[$named] = array('sSelector' => $element,'sFlashSrc' => "http://localhost/massolutions/".$file,'named' => $named);
-		self::set_colours_and_case($named);
-		self::set_padding($named);
-		self::set_position($named);
+	static function disable() {
+		self::$is_disabled = true;
 	}
 
-	static function set_colours_and_case($named = "sifr", $sColor = "#000000", $sLinkColor = "#555555", $bBgColor = "#FFFFFF", $sHoverColor = "#999999", $sWmode = "transparent", $sCase = "") {
-		$array = Array(
-			'sColor' => $sColor,
-			'sLinkColor' => $sLinkColor ,
-			'bBgColor' => $bBgColor,
-			'sHoverColor' => $sHoverColor,
-			'sWmode' => $sWmode,//transparent or opaque
-			'sCase' => $sCase  //lower or upper
-		);
-		self::$fonts[$named] = array_merge(self::$fonts[$named],$array);
+	static function set_js_custom_file_location($folderAndFile) {
+		self::$js_custom_file_location = $folderAndFile;
 	}
 
-  static function set_padding($named = "sifr", $nPaddingTop = 0, $nPaddingBottom = 0, $nPaddingRight = 0, $nPaddingLeft = 0) {
-		$array = Array(
-			'nPaddingTop' => $nPaddingTop,
-			'nPaddingBottom' => $nPaddingBottom ,
-			'nPaddingRight' => $nPaddingRight ,
-			'nPaddingLeft' => $nPaddingLeft ,
-		);
-		self::$fonts[$named] = array_merge(self::$fonts[$named],$array);
+	static function load_sifr() {
+		if(!self::$is_disabled) {
+			Requirements::css("sifr/css/sifr.css", 'screen, projection');
+			Requirements::themedCSS("sifr-themed.css", 'screen, projection');
+			Requirements::javascript("sifr/javascript/sifr.js");
+			Requirements::javascript("sifr/javascript/sifr-debug.js");
+			Requirements::javascript(self::$js_custom_file_location);
+		}
 	}
-
-	static function set_position($named = "sifr", $textalign = "left", $offsetLeft = 0, $offsetTop = 0) {
-		$array = Array(
-			Array(
-				'textalign' => $textalign,
-				'offsetLeft' => $offsetTop ,
-				'offsetTop' => $offsetTop ,
-			)
-		);
-		self::$fonts[$named] = array_merge(self::$fonts[$named],$array);
-	}
-
-
 
 	function is_disabled() {
 		$state = self::$is_disabled;
 		return ($state) ? true : false;
 	}
 
-	/*
-		Disable the sIFR so that you can test without the new fancy fonts
-		around for printing and for the 0.1% of people
-	*/
-	static function disable() {
-		self::$is_disabled = true;
-	}
 
-
-	/*
-		Determines whether the Fragment Identifier Bug will be fixed or not.
-		This is a very peculiar bug in IE where for each Flash movie in the page the fragment identifier
-		part of the URI (#foo) is repeated in the document title. It has been reported by Macromedia that
-		this bug can be fixed by updating the VBScript engine the browser uses. However, for those users
-		who still have the wrong engine sIFR contains a workaround.
-	*/
-	static function disable_fix_frag_id_bug() {
-		self::$b_fix_frag_id_bug = false;
-	}
-	/*
-		Determines whether sIFR will hide the text you want to replace or not.
-	*/
-	static function hide_browser_text() {
-		self::$hide_browser_text = false;
-	}
-
-	function showFonts() {
-		print_r(self::$fonts);
-	}
 }
