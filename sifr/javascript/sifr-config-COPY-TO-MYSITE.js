505e803e4953ebacc11405a0a1f699e522e613a3
Nicolaas <github@sunnysideup.co.nz>
2009-05-13 00:03:23 +0000

    updated sifr-config-COPY-TO-MYSITE.js with a real life example...

--- sifr/javascript/sifr-config-COPY-TO-MYSITE.js
+++ sifr/javascript/sifr-config-COPY-TO-MYSITE.js
@@ -12,37 +12,38 @@ the copyright statement in `sifr.js` is kept intact.
 // Make an object pointing to the location of the Flash movie on your web server.
 // Try using the font name as the variable name, makes it easy to remember which
 // object you're using. As an example in this file, we'll use Futura.
-var futura = { src: '/path/to/futura.swf' };
+var smudger = { src: 'sifr/fonts/Smudger.swf' };
 
 // Now you can set some configuration settings.
 // See also <http://wiki.novemberborn.net/sifr3/JavaScript+Configuration>.
 // One setting you probably want to use is `sIFR.useStyleCheck`. Before you do that,
 // read <http://wiki.novemberborn.net/sifr3/DetectingCSSLoad>.
 
-// sIFR.useStyleCheck = true;
+//sIFR.useStyleCheck = true;
 
 // Next, activate sIFR:
-sIFR.activate(futura);
+sIFR.activate(smudger);
+
 
 // If you want, you can use multiple movies, like so:
 //
 //    var futura = { src: '/path/to/futura.swf' };
 //    var garamond = { src '/path/to/garamond.swf' };
 //    var rockwell = { src: '/path/to/rockwell.swf' };
-//    
+//
 //    sIFR.activate(futura, garamond, rockwell);
 //
 // Remember, there must be *only one* `sIFR.activate()`!
 
 // Now we can do the replacements. You can do as many as you like, but just
 // as an example, we'll replace all `<h1>` elements with the Futura movie.
-// 
+//
 // The first argument to `sIFR.replace` is the `futura` object we created earlier.
 // The second argument is another object, on which you can specify a number of
 // parameters or "keyword arguemnts". For the full list, see "Keyword arguments"
-// under `replace(kwargs, mergeKwargs)` at 
+// under `replace(kwargs, mergeKwargs)` at
 // <http://wiki.novemberborn.net/sifr3/JavaScript+Methods>.
-// 
+//
 // The first argument you see here is `selector`, which is a normal CSS selector.
 // That means you can also do things like '#content h1' or 'h1.title'.
 //
@@ -50,7 +51,8 @@ sIFR.activate(futura);
 // is styled via the `.sIFR-root` class. Here we've specified `background-color`
 // of the entire Flash movie to be a light grey, and the `color` of the text to
 // be red. Read more about styling at <http://wiki.novemberborn.net/sifr3/Styling>.
-sIFR.replace(futura, {
-  selector: 'h1',
-  css: '.sIFR-root { background-color: #F9F9F9; color: #FF0000; }'
+sIFR.replace(smudger, {
+  selector: '#Intro p',
+  css: '.sIFR-root {color: #fff; background-color: transparent; font-size: 23px; leading: -11px;}',
+  wmode: 'transparent'
 });
