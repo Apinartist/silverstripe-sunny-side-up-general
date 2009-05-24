3dc7ca19b1d385edd290066a15608721cd542bca
Nicolaas <github@sunnysideup.co.nz>
2009-05-24 05:00:34 +0000

    add ajax delete option

--- ecommercextras/javascript/ajaxcart.js
+++ ecommercextras/javascript/ajaxcart.js
@@ -3,6 +3,7 @@
 ;(function($) {
 	$(document).ready(function() {
 		ajaxCart.addAddLinks();
+		ajaxCart.addRemoveLinks();
 	});
 
 	var ajaxCart = {
@@ -19,15 +20,29 @@
 					var url = $(this).attr("href");
 					$(this).text(ajaxCart.LoadingText);
 					ajaxCart.loadAjax('CartHolder', url, this);
+
 					return false;
 				}
 			);
+		},
 
+		addRemoveLinks: function () {
+			$(".removeLink").click(
+				function(){
+
+					var url = $(this).attr("href");
+					var el = this;
+					$(el).parent("li").css("text-decoration", "line-through");
+					$.get(url, function(){ $(el).parent("li").fadeOut();});
+					return false;
+				}
+			);
 		},
 
 		loadAjax: function(loadIntoElID, URL, el) {
 			$("#" + loadIntoElID).text(ajaxCart.LoadingText);
-			$("#" + loadIntoElID).load(URL, {}, function() {$(el).text(ajaxCart.BoughText);});
+			$("#" + loadIntoElID).load(URL, {}, function() {$(el).text(ajaxCart.BoughText); ajaxCart.addRemoveLinks();});
+
 			return true;
 		}
 	}
