f16503387e202d7808cbcebd8ee99d1a5e43fc1e
Nicolaas <github@sunnysideup.co.nz>
2009-07-15 10:14:19 +0000

    adding more config and questions

--- simplestspam/_config.php
+++ simplestspam/_config.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * developed by www.sunnysideup.co.nz
+**/
 
-?>
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START sharethis MODULE ----------------===================
+//SpamProtectorManager::set_spam_protector('SimplestSpamProtector');
+//SimplestSpamField::set_question_and_answer("the colour of a banana is ...", "yellow");
+//===================---------------- END sharethis MODULE ----------------===================
