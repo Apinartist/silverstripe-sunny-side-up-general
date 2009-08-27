5369e65deaee62fb1ba7add8ba3a88bdcddfa98a
Nicolaas <github@sunnysideup.co.nz>
2009-08-27 12:41:37 +0000

    adding Product and Product Group Management via modelAdmin

---
+++ ecommercextras/code/modeladmin/ProductsAndGroupsModelAdmin.php
@@ -0,0 +1,14 @@
+<?php
+class ProductsAndGroupsModelAdmin extends ModelAdmin {
+
+	public static $record_controller_class = "ModelAdminRecordControllerForSiteTreeObjects";
+
+	function set_managed_modules($array) {
+		self:::$managed_models = $array;
+	}
+
+	public static $url_segment = 'ecommerce';
+
+	public static $menu_title = 'Ecommerce';
+
+}
