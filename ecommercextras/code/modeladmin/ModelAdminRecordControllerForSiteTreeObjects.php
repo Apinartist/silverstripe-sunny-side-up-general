c58faf0ba349df09b2c0d7f8157b9583430b6a82
Nicolaas <github@sunnysideup.co.nz>
2009-08-27 13:23:50 +0000

    updated comments in ModelAdminRecordControllerForSiteTreeObjects.php

--- ecommercextras/code/modeladmin/ModelAdminRecordControllerForSiteTreeObjects.php
+++ ecommercextras/code/modeladmin/ModelAdminRecordControllerForSiteTreeObjects.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This class replaces the actions in modeladmin for loading pages into modeladmin
+ * the "doSave" function saves and publishes
+ * @package ecommercextras
+ * @subpackage modeladmin
+ */
 
 class ModelAdminRecordControllerForSiteTreeObjects extends ModelAdmin_RecordController{
 
@@ -14,25 +20,21 @@ class ModelAdminRecordControllerForSiteTreeObjects extends ModelAdmin_RecordCont
 	public function EditForm() {
 		$form = parent::EditForm();
 		$oldActions = $form->Actions();
-		//$form->unsetAllActions();
 		//in order of appearance
-		//$form->unsetActionByName("action_doDelete");
+		//$form->unsetActionByName("action_doDelete"); - USEFUL TO KEEP
 		$form->unsetActionByName("action_unpublish");
 		$form->unsetActionByName("action_delete");
 		$form->unsetActionByName("action_save");
 		$form->unsetActionByName("action_publish");
-		//$form->unsetActionByName("action_doSave");
+		//$form->unsetActionByName("action_doSave"); - USEFUL TO KEEP
 		return $form;
 	}
 
 	function doSave($data, $form, $request) {
 		$form->saveInto($this->currentRecord);
-		$this->currentRecord->write();
 		$this->currentRecord->writeToStage("Stage");
 		$this->currentRecord->publish("Stage", "Live");
 		$this->currentRecord->flushCache();
-
-		// Behaviour switched on ajax.
 		if(Director::is_ajax()) {
 			return $this->edit($request);
 		} else {
@@ -43,6 +45,7 @@ class ModelAdminRecordControllerForSiteTreeObjects extends ModelAdmin_RecordCont
 
 	function doDelete() {
 		user_error("this function has not been implemented yet", E_USER_NOTICE);
+		//might be prudent not to allow deletions as products should not be deleted, but rather be made "not for sale"
 	}
 
 }
