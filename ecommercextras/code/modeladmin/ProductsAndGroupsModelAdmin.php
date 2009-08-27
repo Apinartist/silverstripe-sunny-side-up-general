9a81ee58e8b08e7914899919c6507e7749335998
Nicolaas <github@sunnysideup.co.nz>
2009-08-27 12:50:40 +0000

    adding model admin to _config.php

--- ecommercextras/code/modeladmin/ProductsAndGroupsModelAdmin.php
+++ ecommercextras/code/modeladmin/ProductsAndGroupsModelAdmin.php
@@ -3,7 +3,7 @@ class ProductsAndGroupsModelAdmin extends ModelAdmin {
 
 	public static $record_controller_class = "ModelAdminRecordControllerForSiteTreeObjects";
 
-	static function set_managed_modules($array) {
+	public static function set_managed_models($array) {
 		self::$managed_models = $array;
 	}
 
