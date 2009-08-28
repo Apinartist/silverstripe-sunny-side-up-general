100a82d8c87b4516a22ed00f2388ef4647e091d3
Nicolaas <github@sunnysideup.co.nz>
2009-08-28 15:11:54 +0000

    added stricter rules before showing map

--- googlemap/code/decorator/GoogleMapLocationsDOD.php
+++ googlemap/code/decorator/GoogleMapLocationsDOD.php
@@ -72,7 +72,7 @@ class GoogleMapLocationsDOD extends DataObjectDecorator {
 	}
 
 	public function hasMap() {
-		if($this->map) {
+		if($this->map && $this->owner->HasGeoInfo) {
 			return true;
 		}
 		else {
