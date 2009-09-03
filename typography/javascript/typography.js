438c0292ad08ae23c9436a3769bbd915ac4ba964
Nicolaas <github@sunnysideup.co.nz>
2009-09-03 10:57:28 +0000

    update colour table stuff....

--- typography/javascript/typography.js
+++ typography/javascript/typography.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(
 	function () {
-		jQuery(".typography img").css("heigth", "100px");
-		jQuery(".typography img").css("width", "200px");
+		jQuery(".typography img").css("heigth", "60px");
+		jQuery(".typography img").css("width", "100px");
 
-		for(var i = 1; i < 11; i++) {
-			var className = ".colour" + i;
+		for(var i = 1; i < 6; i++) {
+			var className = ".backgroundcolour" + i;
 			var colourName = jQuery(className).css("background-color");
 			jQuery(className).html("<span style=\'color: #fff;\'>" + colourName + "<\/span> <span style=\'color: #000;\'>" + colourName + "<\/span>");
 		}
