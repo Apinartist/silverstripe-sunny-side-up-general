74304870729c97e05f00058a6f953caa94bf596b
Nicolaas <github@sunnysideup.co.nz>
2009-09-04 05:28:46 +0000

    small fsmall formatting issue in thickbox/_config.php

--- thickbox/_config.php
+++ thickbox/_config.php
@@ -4,8 +4,6 @@
  * developed by www.sunnysideup.co.nz
  * author: Nicolaas - modules [at] sunnysideup.co.nz
  * to get it going include: ThickBox::include_code() in init function
- *
- *
  **/
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
