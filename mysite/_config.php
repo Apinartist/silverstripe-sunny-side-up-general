223b6228067c860df515a93e5b1e779f4b580453
Nicolaas <github@sunnysideup.co.nz>
2009-09-04 05:02:59 +0000

    fixed typo in _config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -32,7 +32,7 @@ MetaTagAutomation::set_update_meta_title(0);
 MetaTagAutomation::set_prepend_to_meta_title("");
 MetaTagAutomation::set_append_to_meta_title("");
 /* meta descriptions */
-MetaTagAutomation::set_update_meta_desc(0)
+MetaTagAutomation::set_update_meta_desc(0);
 MetaTagAutomation::set_meta_desc_length(12);
 /* meta keywords */
 MetaTagAutomation::set_update_meta_keys(0);
