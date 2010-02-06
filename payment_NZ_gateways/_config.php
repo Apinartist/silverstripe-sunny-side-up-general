df82ac40f8cb3d62840cf317ce22a08b4eed85d9
Nicolaas <github@sunnysideup.co.nz>
2010-02-06 04:09:59 +0000

    SecurePayTech gateway.

--- payment_NZ_gateways/_config.php
+++ payment_NZ_gateways/_config.php
@@ -7,4 +7,6 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START payment_NZ_gateways MODULE ----------------===================
 //DirectCreditPayment::set_custom_message_for_direct_credit("your bank account here");
+SecurePayTechPayment::set_spt_merchant_id ('TESTDIGISPL1');
+SecurePayTechPayment::set_spt_merchant_key ('d557591484cb2cd12bba445aba420d2c69cd6a88');
 //===================---------------- END payment_NZ_gateways MODULE ----------------===================
