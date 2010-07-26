ea12b61932136dd324fe1d8e9f8afd7afb6489a0
Nicolaas <github@sunnysideup.co.nz>
2010-07-26 04:06:02 +0000

    word cloud js code refreshing

--- google_charts/javascript/wordcloud.js
+++ google_charts/javascript/wordcloud.js
@@ -1,24 +1,26 @@
-google.load('visualization', '1', {packages:['table']});
+google.load('visualization', '1', {packages: ['table']});
 
-google.setOnLoadCallback(draw_$id);
+google.setOnLoadCallback(drawChart_$id);
 
-function draw_$id() {
+function drawChart_$id() {
 	
-	data = new google.visualization.DataTable();
+	var data = new google.visualization.DataTable();
+	
 	data.addColumn('string', 'Label');
 	data.addColumn('number', 'Value');
 	data.addColumn('string', 'Link');
 	
 	var words = [$words];
 	var fontsizes = [$fontsizes];
+	
 	data.addRows(words.length);
+	
 	for(var i = 0; i < words.length; i++) {
 		data.setCell(i, 0, words[i]);
 		data.setCell(i, 1, fontsizes[i]);
 		data.setCell(i, 2, '');
 	}
 	
-	var outputDiv = document.getElementById('$id');
-	var tc = new TermCloud(outputDiv);
-	tc.draw(data, null);
+	var chart = new TermCloud(document.getElementById('$id'));
+	chart.draw(data, null);
 }
