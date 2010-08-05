101606eae1e93931b221fe18c6ca3354232a8c29
Nicolaas <github@sunnysideup.co.nz>
2010-08-05 02:49:22 +0000

    tasks without CRON first commit

--- tasks_without_cron/_config.php
+++ tasks_without_cron/_config.php
@@ -6,10 +6,9 @@
 
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ START tasks_without_cron MODULE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//TaskWithoutCron::set_expected_visitors_per_day(10);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END tasks_without_cron MODULE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//===================---------------- START superfish MODULE ----------------===================
 
+//===================---------------- END slideshow MODULE ----------------===================
 
 
 
