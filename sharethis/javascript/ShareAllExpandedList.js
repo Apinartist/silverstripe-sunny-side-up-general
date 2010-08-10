50cff8aa944e22351c0db6c04676700a5f3f6657
Nicolaas <github@sunnysideup.co.nz>
2010-08-10 00:57:10 +0000

    improved JS for ShareAllExpandedList.js

--- sharethis/javascript/ShareAllExpandedList.js
+++ sharethis/javascript/ShareAllExpandedList.js
@@ -18,12 +18,14 @@ var ShareAllExpandedList = {
 		jQuery(".shareAllExpandedListUL").css("display", "none");
 		jQuery(".shareAllExpandedListHeader a").click(
 			function() {
-				if(jQuery(".shareAllExpandedListUL").is(":hidden")) {
+				if(jQuery(this).parent().parent().siblings(".shareAllExpandedListUL").is(":hidden")) {
 					jQuery(this).addClass("listShown");
+					jQuery(this).removeClass("listHidden");
 					jQuery(this).parent().parent().siblings(".shareAllExpandedListUL").slideDown("slow");
 				}
 				else {
 					jQuery(this).addClass("listHidden");
+					jQuery(this).removeClass("listShown");
 					jQuery(this).parent().parent().siblings(".shareAllExpandedListUL").slideUp("fast");
 				}
 				return false;
