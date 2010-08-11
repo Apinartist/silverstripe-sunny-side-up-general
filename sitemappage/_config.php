1f234ef12a280f25dfe7bec8eb3bdaf8eb637475
Nicolaas <github@sunnysideup.co.nz>
2010-08-11 15:47:39 +0000

    fixed bug in description in sitemappage/_config.php

--- sitemappage/_config.php
+++ sitemappage/_config.php
@@ -10,7 +10,7 @@
 /*
 	function SiteMapPages() {
 		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
-		return DataObject::get("SiteTree", "{$bt}ShowInMenus{$bt} = 1 AND {$bt}ShowInSearch{$bt} = 1 AND {$bt}ParentID{$bt} = ".$this->owner->ID." AND {$bt}ClassName{$bt} <> 'SiteMapPage'");
+		return DataObject::get("SiteTree", "{$bt}ShowInMenus{$bt} = 1 AND {$bt}ShowInSearch{$bt} = 1 AND {$bt}ParentID{$bt} = ".$this->ID." AND {$bt}ClassName{$bt} <> 'SiteMapPage'");
 	}
 */
 //===================---------------- END sitemappage MODULE ----------------===================
