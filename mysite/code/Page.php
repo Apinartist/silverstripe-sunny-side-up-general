7c82d5986249eb34702cf7761c612d424a45b399
Nicolaas <github@sunnysideup.co.nz>
2010-09-21 04:03:54 +0000

    removed search from mysite/code/Page.php

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -16,33 +16,6 @@ class Page extends SiteTree {
 	function requireDefaultRecords() {
 		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
 		parent::requireDefaultRecords();
-		if(!DataObject::get("Member")) {
-			$member = new Member();
-			$member->FirstName = "Sunny";
-			$member->Surname = 'Tester';
-			$member->Email = 'tester@sunnysideup.co.nz';
-			$member->write();
-			$member = DataObject::get_one("Member", "Email = 'tester@sunnysideup.co.nz'");
-			if($member) {
-				$member->Password = 'lovesSun33'; // support for up to SHA256!
-				$member->RememberLoginToken = true;
-			}
-		}
-		else {
-			$member = DataObject::get_one("Member", "Email = 'tester@sunnysideup.co.nz'");
-			$group = DataObject::get_by_id("Group", 1);
-			if($group && $member) {
-				if(!$member->IsAdmin()) {
-					Group::addToGroupByName($member, $group->Code);
-					$member->write();
-				}
-				if($dos = DataObject::get_one("Member", "Email <> 'tester@sunnysideup.co.nz'")) {
-					if($member) {
-						$member->delete();
-					}
-				}
-			}
-		}
 		$page = DataObject::get_one("Page", "{$bt}URLSegment{$bt} = 'admin-only'");
 		if(!$page) {
 			$page = new Page();
@@ -63,26 +36,6 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 		$this->addBasicMetatagRequirements();
-	}
-
-	function SearchForm() {
-		$searchText = isset($_REQUEST['Search']) ? $_REQUEST['Search'] : 'Search';
-		$fields = new FieldSet(
-			new TextField("Search", "", $searchText)
-		);
-		$actions = new FieldSet(
-			new FormAction('results', 'Search')
-		);
-  	return new SearchForm($this, "SearchForm", $fields, $actions);
-	}
-
-	function results($data, $form){
-		$data = array(
-			'Results' => $form->getResults(),
-			'Query' => $form->getSearchQuery(),
-			'Title' => 'Search Results'
-		);
-		return $this->customise($data)->renderWith(array('Page_results', 'Page'));
 	}
 
 }
