41a814d87f35d3dc7539f3f71bb3e99c94f291e4
Nicolaas <github@sunnysideup.co.nz>
2010-09-23 00:35:29 +0000

    added scenario where total = 0

--- google_charts/code/PieChart.php
+++ google_charts/code/PieChart.php
@@ -2,26 +2,26 @@
 
 /**
  * This class is used to create a pie chart as an image using google charts
- * 
+ *
  * @link http://code.google.com/apis/chart/docs/gallery/pie_charts.html
  * @see Chart
  * @package googlecharts
  * @author Romain Louis <romain@sunnysideup.co.nz>
  */
 class PieChart extends Chart {
-	
+
 	protected $type = 'p';
-	
+
 	static $types = array('p', 'p3', 'pc');
-	
+
 	protected $portions = array();
-	
+
 	static $labels_param = 'chl';
 	static $rotation_param = 'chp';
-	
+
 	protected $colors = array();
 	protected $rotation;
-	
+
 	function addPortion($portion, $color = null, $legend = null, $label = null) {
 		$portion = array('value' => $portion);
 		if($color) $portion['color'] = $color;
@@ -29,7 +29,7 @@ class PieChart extends Chart {
 		if($label) $portion['label'] = $label;
 		$this->portions[] = $portion;
 	}
-	
+
 	function Link(array $params = null) {
 		if(count($this->portions) > 0) {
 			foreach($this->portions as $portion) {
@@ -40,6 +40,9 @@ class PieChart extends Chart {
 				if(isset($portion['label'])) $label[] = str_replace(' ', '+', $portion['label']);
 			}
 			$total = array_sum($portions);
+			if($total == 0) {
+				$total = 1;
+			}
 			foreach($portions as $i => $portion) $portions[$i] = round($portion * 100 / $total);
 			$params[Chart::$data_param] = 't:' . implode(',', $portions);
 			if(isset($colors)) $params[Chart::$color_param] = implode(count($colors) == count($this->portions) ? '|' : ',', $colors);
@@ -50,85 +53,85 @@ class PieChart extends Chart {
 		}
 		return parent::Link($params);
 	}
-	
+
 	function setColor($color1, $color2 = null) {
 		$this->colors[0] = $color1;
 		if($color2) $this->colors[1] = $color2;
 	}
-	
+
 	function setRotation($rotation) {$this->rotation = $rotation;}
 }
 
 /**
  * This class is used to create an interactive pie chart using javascript
- * 
+ *
  * @link http://code.google.com/apis/visualization/documentation/gallery/piechart.html
  * @see PieChart
  * @package googlecharts
  * @author Romain Louis <romain@sunnysideup.co.nz>
  */
 class PieChart_Interactive extends PieChart {
-	
+
 	static $legend_positions = array('left', 'right', 'none');
-	
+
 	protected $backgroundColor;
 	protected $fontSize;
-	
+
 	protected $legendFontSize, $legendColor;
 	protected $reverseCategories;
 	protected $tooltipFontSize;
-	
+
 	function __construct() {
 		parent::__construct();
 		Requirements::javascript('http://www.google.com/jsapi');
 	}
-	
+
 	function forTemplate() {
 		$id = "PI_$this->id";
 		$params['id'] = $id;
-		
+
 		foreach($this->portions as $portion) {
 			$titles[] = $portion['legend'];
 			$values[] = $portion['value'];
 			if(isset($portion['color'])) $colors[] = $portion['color'];
 		}
-		
+
 		$params['titles'] = "'" . implode("', '", $titles) . "'";
 		$params['values'] = implode(', ', $values);
-		
+
 		$options[] = 'width: ' . ($this->width ? $this->width : Chart::$default_width);
 		$options[] = 'height: ' . ($this->height ? $this->height : Chart::$default_height);
-		
+
 		if(isset($colors)) $options[] = "colors: ['" . implode("', '", $colors) . "']";
-		
+
 		if($this->title) $options[] = "title: '$this->title'";
 		if($this->titleColor) $options[] = "titleTextColor: '$this->titleColor'";
 		if($this->titleSize) $options[] = "titleFontSize: '$this->titleSize'";
-		
+
 		if($this->backgroundColor) $options[] = "backgroundColor: '$this->backgroundColor'";
 		if($this->fontSize) $options[] = "fontSize: '$this->fontSize'";
-		
+
 		if($this->legendPosition) $options[] = "legend: '$this->legendPosition'";
 		if($this->legendFontSize) $options[] = "legendFontSize: '$this->legendFontSize'";
 		if($this->legendColor) $options[] = "legendTextColor: '$this->legendColor'";
-		
+
 		if($this->reverseCategories) $options[] = "reverseCategories: true";
-		
+
 		if($this->tooltipFontSize) $options[] = "tooltipFontSize: $this->tooltipFontSize";
-		
+
 		$params['options'] = implode(', ', $options);
 		Requirements::javascriptTemplate('googlecharts/javascript/pie.js', $params);
 		return "<div id=\"$id\" class=\"pie\"></div>";
 	}
-	
+
 	function setBackgroundColor($color) {$this->backgroundColor = $color;}
 	function setFontSize($size) {$this->fontSize = $size;}
-	
+
 	function setLegendFontSize($size) {$this->legendFontSize = $size;}
 	function setLegendColor($color) {$this->legendColor = $color;}
-	
+
 	function reverseCategories() {$this->reverseCategories = true;}
-	
+
 	function setTooltipFontSize($size) {$this->tooltipFontSize = $size;}
 }
 
