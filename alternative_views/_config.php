dc7370dc428c5b1976de54134567238672de0b31
Nicolaas <github@sunnysideup.co.nz>
2010-10-12 05:23:42 +0000

    basic concept for Alternative Views

---
+++ alternative_views/_config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *@author nicolaas[at] sunnysideup.co.nz
+ *
+ *
+ **/
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START alternative_views MODULE ----------------===================
+//Object::add_extension('SiteTree', 'AlternativeViews');
+//===================---------------- END alternative_views MODULE ----------------===================
---
+++ alternative_views/code/model/AlternativeViews.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *@author nicolaas [at] sunnysideup.co.nz
+ *
+ *
+ **/
+
+class AdvertisementDecorator extends DataObjectDecorator {
+
+
+
+}
