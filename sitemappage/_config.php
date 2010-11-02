d19b9f6d3d20d4fe928f5427ff331a8e90bb3374
Nicolaas <github@sunnysideup.co.nz>
2010-11-02 12:50:04 +0000

    improved sitemap

--- sitemappage/_config.php
+++ sitemappage/_config.php
@@ -6,11 +6,10 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START sitemappage MODULE ----------------===================
-// ADD THE FOLLOWING FUNCTION TO YOUR PAGE CLASS (NOT PAGE_CONTROLLER!)
-/*
-	function SiteMapPages() {
-		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
-		return DataObject::get("SiteTree", "{$bt}ShowInMenus{$bt} = 1 AND {$bt}ShowInSearch{$bt} = 1 AND {$bt}ParentID{$bt} = ".$this->ID." AND {$bt}ClassName{$bt} <> 'SiteMapPage'");
-	}
-*/
+//MUST SET
+//Object::add_extension('SiteTree', 'SitemapPageDecorator');
+
+// OPTIONAL - to control what pages are shown in sitemap... INCLUDES overrules EXCLUDE
+//SitemapPageDecorator::set_sitemap_classes_to_exclude(array("HomePage", "MenuPage")); OR
+//SitemapPageDecorator::set_sitemap_classes_to_include(array("ProductPage", "CarrotPage"));
 //===================---------------- END sitemappage MODULE ----------------===================
--- sitemappage/code/decorators/SitemapPageDecorator.php
+++ sitemappage/code/decorators/SitemapPageDecorator.php
@@ -2,9 +2,26 @@
 
 class SitemapPageDecorator extends SiteTreeDecorator {
 
+	protected static $sitemap_classes_to_exclude = array();
+		static function set_sitemap_classes_to_exclude($v) {self::$sitemap_classes_to_exclude = $v;}
+		static function get_sitemap_classes_to_exclude() {return self::$sitemap_classes_to_exclude;}
+
+	protected static $sitemap_classes_to_include = array();
+		static function set_sitemap_classes_to_include($v) {self::$sitemap_classes_to_include = $v;}
+		static function get_sitemap_classes_to_include() {return self::$sitemap_classes_to_include;}
+
 	function SiteMapPages() {
 		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
-		return DataObject::get("SiteTree", '{$bt}ShowInMenus{$bt} = 1 AND {$bt}ShowInSearch{$bt} = 1 AND {$bt}ParentID{$bt} = '.$this->owner->ID.' AND {$bt}ClassName{$bt} <> "SiteMapPage"');
+		$where = '1 = 1';
+		$inc = self::get_sitemap_classes_to_include();
+		$exc = self::get_sitemap_classes_to_include();
+		if(is_array($inc) && count($inc)) {
+			$where = "{$bt}ClassName{$bt} IN ('".implode("','", $inc)."')";
+		}
+		elseif(is_array($inc) && count($inc)) {
+			$where = "{$bt}ClassName{$bt} NOT IN ('".implode("','", $inc)."')";
+		}
+		return DataObject::get("SiteTree", '{$bt}ShowInMenus{$bt} = 1 AND {$bt}ShowInSearch{$bt} = 1 AND {$bt}ParentID{$bt} = '.$this->owner->ID.' AND {$bt}ClassName{$bt} <> "SiteMapPage" AND '.$where);
 	}
 
 }
