e024b073c78783635dd1f90b4295fcac4f7b7e4f
Nicolaas <github@sunnysideup.co.nz>
2010-11-07 13:45:27 +0000

    remodelling for _config.js

--- advertisements/_config.php
+++ advertisements/_config.php
@@ -10,9 +10,18 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START advertisements MODULE ----------------===================
+Director::addRules(10, array(
+	'advertisements//$Action/$ID' => 'AdvertisementController',
+));
 //**** NB, dont forget to theme the templates (and also JS, as shown below!)
-//Object::add_extension('SiteTree', 'SlideShowDecorator');
+//MUST SET
+//Object::add_extension('SiteTree', 'AdvertisementDecorator');
+//MAY SET
 //AdvertisementDecorator::set_use_custom_javascript(false); //if true, this will search for myproject/javascript/SlideShow.js myproject is usually called mysite.
-//AdvertisementDecorator::set_page_classes_without_slideshow(array("UserDefineForm", "ErrorPage")); // excluded from the specified classes
-//AdvertisementDecorator::set_page_classes_with_slideshow(array("HomePage")); //ONLY shown on specified classes
+//AdvertisementDecorator::set_page_classes_without_advertisements(array("UserDefineForm", "ErrorPage")); // excluded from the specified classes
+//AdvertisementDecorator::set_page_classes_with_advertisements(array("HomePage")); //ONLY shown on specified classes
+//Advertisement::set_width(100);
+//Advertisement::set_height(100);
+//AdvertisementStyle::add_to_array_of_js_file_options("slideshow style 1", "mysite/javascript/slideshow1.js");
+//AdvertisementStyle::add_to_array_of_js_file_options("slideshow style 2", "mysite/javascript/slideshow1.js");
 //===================---------------- END advertisements MODULE ----------------===================
