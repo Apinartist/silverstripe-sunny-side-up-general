032fbaa959cf8607d22dd31af3770e511c8b0518
Nicolaas <github@sunnysideup.co.nz>
2010-11-22 16:18:02 +0000

    basic flow player is ready.

---
+++ flowplayer/_config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+*@author Nicolaas [at] sunnysideup.co.nz
+*
+**/
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START flowplayer MODULE ----------------===================
+//FlowPlayer::set_flow_player_config_file("mysite/javascript/FlowPlayerConfig.js");
+//===================---------------- END flowplayer MODULE ----------------===================
---
+++ flowplayer/code/FlowPlayer.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+  *@author nicolaas [at] sunnysideup.co.nz
+  **/
+
+class FlowPlayer extends File {
+
+	protected static $flow_player_config_file = "flowplayer/javascript/FlowPlayerConfig.js";
+		static function set_flow_player_config_file($v) {self::$flow_player_config_file = $v;}
+		static function get_flow_player_config_file() {return self::$flow_player_config_file;}
+
+	function AbsoluteLink(){
+		Requirements::javascript("flowplayer/thirdparty/flowplayer-3.2.4.min.js");
+		Requirements::javascript(self::get_flow_player_config_file());
+		Requirements::customScript('flowplayer("FlowPlayer", "flowplayer/thirdparty/flowplayer-3.2.5.swf", FlowPlayerConfig);', "FlowPlayerSWF");
+		Requirements::themedCSS("FlowPlayer");
+		return $this->getAbsoluteURL();
+	}
+
+
+
+}
