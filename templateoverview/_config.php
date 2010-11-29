1d4c23017a475ec0be6ed48f0cf2334fa91d3601
Nicolaas <github@sunnysideup.co.nz>
2010-11-29 10:15:34 +0000

    adding error reporting to template overview

--- templateoverview/_config.php
+++ templateoverview/_config.php
@@ -10,12 +10,16 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START templateoverview MODULE ----------------===================
+/*
 //MUST SET
-//if(Director::isDev()) {Object::add_extension('Page_Controller', 'TemplateOverviewPageExtension');}
+if(Director::isDev()) {
+  Object::add_extension('Page_Controller', 'TemplateOverviewPageExtension');
 //MAY SET
-//if(Director::isDev()) {Object::add_extension('SiteTree', 'TemplateOverviewPageDecorator');}
-if(Director::isDev()) {Director::addRules(7, array('error/report' => 'ErrorNotifierController'));}
-//TemplateOverviewPage::set_auto_include(true);
-//LeftAndMain::require_css("templateoverview/css/TemplateOverviewCMSHelp.css");
+  Object::add_extension('SiteTree', 'TemplateOverviewPageDecorator');}
+  Director::addRules(7, array('error/report' => 'ErrorNotifierController'));
+ }
+TemplateOverviewPage::set_auto_include(true);
+LeftAndMain::require_css("templateoverview/css/TemplateOverviewCMSHelp.css");
+*/
 //===================---------------- END templateoverview MODULE ----------------===================
 
