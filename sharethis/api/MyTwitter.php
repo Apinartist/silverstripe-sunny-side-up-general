abb625e0ecbf90629c57dbe0aa038107500788ed
Nicolaas <github@sunnysideup.co.nz>
2010-12-14 20:06:51 +0000

    twitter function addinh

--- sharethis/api/MyTwitter.php
+++ sharethis/api/MyTwitter.php
@@ -7,5 +7,12 @@
 
 class MyTwitter extends RestfulService {
 
-
+	static function last_statuses($username, $count = null) {
+		$url = "http://twitter.com/statuses/user_timeline/$username.xml";
+		if($count) $url = HTTP::setGetVar('count', $count, $url);
+		$content = file_get_contents($url);
+		if($content) {
+			return new SimpleXMLElement($content);
+		}
+	}
 }
