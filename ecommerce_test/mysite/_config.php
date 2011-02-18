1321dd72b777e659b1b44640d1f680d2809c226a
Nicolaas <github@sunnysideup.co.nz>
2011-02-18 14:36:52 +0000

    updated mysite/_config.php

--- ecommerce_test/mysite/_config.php
+++ ecommerce_test/mysite/_config.php
@@ -15,47 +15,47 @@ SiteTree::enable_nested_urls();
 
 
 // __________________________________START ECOMMERCE MODULE CONFIG __________________________________
+
+
+// copy the lines below to your mysite/_config.php file and set as required.
+// __________________________________START ECOMMERCE MODULE CONFIG __________________________________
 //The configuration below is not required, but allows you to customise your ecommerce application - check for the defalt value first.
 // * * * DEFINITELY MUST SET
 //Order::set_modifiers(array("MyModifierOne", "MyModifierTwo");
 
-// * * * HIGHLY RECOMMENDED SETTINGS NON-ECOMMERCE
-Payment::set_site_currency('NZD');
-Geoip::$default_country_code = "NZ";
-Object::add_extension("Product", "Buyable");
 
+// * * * HIGHLY RECOMMENDED SETTINGS NON-ECOMMERCE
+//Payment::set_site_currency('NZD');
+Geoip::$default_country_code = 'NZ';
+
+// * * * ECOMMERCE I18N SETTINGS
+// * * * for Currency &  Date Formats get this module: http://code.google.com/p/silverstripe-i18n-fieldtypes/
+//Object::useCustomClass('Currency','I18nCurrency',true);
+//Object::useCustomClass('Money','CustomMoney',true);
+// * * * FOR DATE FORMATS SET F.E.
+//setlocale (LC_TIME, 'en_NZ@dollar', 'en_NZ.UTF-8', 'en_NZ', 'nz', 'nz');
+//Object::useCustomClass('SS_Datetime','I18nDatetime',true);
+//OR
+//i18n::set_locale('en_NZ');
+//Object::useCustomClass('SS_Datetime','ZendDate',true);
+//Currency::setCurrencySymbol("€");
 
 // * * * SHOPPING CART AND ORDER
-CheckoutPage::set_add_shipping_fields(true);
-//ShoppingCart::set_fixed_country_code("NZ"); //always use the same country code
-//Order::set_table_overview_fields(array('Total' => 'Total','Status' => 'Status'));//
+//EcomQuantityField::set_hide_plus_and_minus(true);
+//Order::set_add_shipping_fields(true);
+//Order::set_table_overview_fields(array('Total' => 'Total','Status.Name'));//
 //Order::set_maximum_ignorable_sales_payments_difference(0.001);//sometimes there are small discrepancies in total (for various reasons)- here you can set the max allowed differences
 //Order::set_order_id_start_number(1234567);//sets a start number for order ID, so that they do not start at one.
-//Order::set_cancel_before_payment(false); //soon to be depreciated
-//Order::set_cancel_before_processing(false); //soon to be depreciated
-//Order::set_cancel_before_sending(false); //soon to be depreciated
-//Order::set_cancel_after_sending(false); //soon to be depreciated
-
-// * * * PRODUCTS
-//ProductsAndGroupsModelAdmin::set_managed_models(array(("Product", "ProductGroup","ProductVariation"));
-//SS_Report::register("SideReport", "EcommerceSideReport_AllProducts");
-//SS_Report::register("SideReport", "EcommerceSideReport_FeaturedProducts");
-//SS_Report::register("SideReport", "EcommerceSideReport_NoImageProducts");
-//Product_Image::set_thumbnail_size(140, 100);
-//Product_Image::set_content_image_width(200);
-//Product_Image::set_large_image_width(200);
-//ProductGroup::set_include_child_groups(true);
-//ProductGroup::set_must_have_price(true);
-//ProductGroup::set_sort_options( array('Title' => 'Alphabetical','Price' => 'Lowest Price')); // will be depreciated soon in this form, WATCH THIS SPACE.
-
 
 // * * * CHECKOUT
 //ExpiryDateField::set_short_months(true); //uses short months (e.g. Jan instead of january) for credit card expiry date.
-//EcommerceRole::set_fixed_country_code("NZ"); //country is fixed
-//EcommerceRole::set_login_invite_alternative_text('<a href="http://www.mysite.com/Security/login/?BackURL=">If you are a member then please log in.</a>); //label for link that can be used to check postal code
 
 // * * * MEMBER
-EcommerceRole::set_group_name("SunnySideUp Customers");
+//EcommerceRole::set_customer_group_name("Customers");
+//EcommerceRole::set_admin_group_name("Shop Administrators");
+//EcommerceRole::set_fixed_country_code("NZ"); //country is fixed
+//EcommerceRole::set_allowed_country_codes("NZ" => "NZ", "UK," => "UK"); //country is fixed
+//EcommerceRole::set_login_invite_alternative_text('<a href="http://www.mysite.com/Security/login/?BackURL=">If you are a member then please log in.</a>);
 
 // * * * MODIFIERS
 //FlatTaxModifier::set_tax("0.15", "GST", $exclusive = false);
@@ -63,9 +63,44 @@ EcommerceRole::set_group_name("SunnySideUp Customers");
 //SimpleShippingModifier::::set_charges_for_countries(array('US' => 10,'NZ' => 5));
 //TaxModifier::::set_for_country($country = "NZ", $rate = 0.15, $name = "GST", $inclexcl = "inclusive"))
 
+// * * * SPECIAL CASES
+//OrderItem::disable_quantity_js();
+//ShoppingCartset_response_class("EcommerceResponse");
+
+// * * * PRODUCTS
+//ProductsAndGroupsModelAdmin::set_managed_models(array(("Product", "ProductGroup");
+//SS_Report::register("SideReport", "EcommerceSideReport_AllProducts");
+//SS_Report::register("SideReport", "EcommerceSideReport_FeaturedProducts");
+//SS_Report::register("SideReport", "EcommerceSideReport_NoImageProducts");
+//Product_Image::set_thumbnail_size(140, 100);
+//Product_Image::set_content_image_width(200);
+//Product_Image::set_large_image_width(200);
+//ProductGroup::set_include_child_groups(true);
+//ProductGroup::only_show_products_that_can_purchase(true);
+//ProductGroup::add_sort_option( $key = "price", $title = "Lowest Price", $sql = "Price ASC");
+//ProductGroup::remove_sort_option( $key = "title");
+//ProductGroup::set_sort_options_default( $key = "price");
+//ProductGroup::set_only_show_products_that_can_purchase(true);
+
+// * * * EMAILS
+//Order_Email::set_send_all_emails_plain(true);
+//Order_Email::set_copy_to_admin_for_all_emails(false);
+
+// * * * LOGS
+//OrderStatusLog::set_available_log_classes_array(array("OrderStatusLog_PaymentCheck"));
+
 // * * * HELP
-//Product::set_global_allow_purcahse(false); //stops the sale of all products
-// ------------------------------------------------------END ECOMMERCE MODULE CONFIG ------------------------------------------------------
+// __________________________________ END ECOMMERCE MODULE CONFIG __________________________________
+
+
+
+// __________________________________ END PAYMENT MODULE CONFIG __________________________________
+Payment::set_site_currency("NZD");
+
+Payment::set_supported_methods(array(
+	'PayPalPayment' => 'Paypal Payment'
+));
+// __________________________________ END PAYMENT MODULE CONFIG __________________________________
 
 
 
