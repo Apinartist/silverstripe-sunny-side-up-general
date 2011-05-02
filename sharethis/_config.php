45ae2aa5df8d787ff9461b98d1970d53cc24c487
Nicolaas <github@sunnysideup.co.nz>
2011-05-02 05:14:40 +0000

    adding facebook connect

--- sharethis/_config.php
+++ sharethis/_config.php
@@ -7,6 +7,7 @@
  *
  **/
 
+define('SHARETHIS_DIR', 'sharethis');
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START sharethis MODULE ----------------===================
