145a91ecfc996d5b48a2de7882951666de63e83c
Nicolaas <github@sunnysideup.co.nz>
2011-06-08 12:45:45 +0000

    adding designers module.

--- designers/code/DesignersPage.php
+++ designers/code/DesignersPage.php
@@ -32,7 +32,7 @@ class DesignersPage_Controller extends Page_Controller {
 
 	function init() {
 		parent::init();
-		PrettyPhoto::include();
+		PrettyPhoto::include_code();
 		PrettyPhoto::set_theme("dark_rounded");//"dark_rounded", "dark_square", "facebook", "light_rounded", "light_square" OR create your own!
 		PrettyPhoto::set_more_config("AnimationSpeed: 'slow', opacity: 0.30, showTitle: false");//see readme for some examples!
 	}
