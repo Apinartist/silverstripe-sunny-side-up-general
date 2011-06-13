58d0552e79f4511dfa976b5bd9a030d3b242f126
Nicolaas <github@sunnysideup.co.nz>
2011-06-13 04:14:40 +0000

    pie charts updates

--- google_charts/code/Chart.php
+++ google_charts/code/Chart.php
@@ -24,11 +24,13 @@ class Chart extends ViewableData {
 	static $title_style_param = 'chts';
 	static $data_scaling_param = 'chds';
 	static $legend_labels_param = 'chdl';
-	static $legend_settings_param = 'chdlp';
+	static $legend_position_param = 'chdlp';
+	static $legend_style_param = 'chdls';
 	static $margin_param = 'chma';
 	static $visible_axes_param = 'chxt';
 	static $axis_range_param = 'chxr';
 	static $axis_labels_param = 'chxl';
+	static $background_fill_param = 'chf';
 	
 	static $legend_positions = array('b', 'bv', 't', 'tv', 'r', 'l');
 	static $legend_orders = array('l', 'r', 'a');
@@ -37,10 +39,11 @@ class Chart extends ViewableData {
 	protected $type;
 	protected $width, $height;
 	protected $generateColor = false;
-	protected $title, $titleColor, $titleSize;
-	protected $legendPosition, $legendOrder;
+	protected $title, $titleColor, $titleSize, $showTitle = true;
+	protected $legendPosition, $legendOrder, $legendColor, $legendFontSize, $showLegend = true;
 	protected $marginLeft, $marginRight, $marginTop, $marginBottom, $legendWidth, $legendHeight;
 	protected $visibleAxes, $axisRange, $axisLabels;
+	protected $backgroundColor, $chartColor, $transparency;
 	
 	static $cpt = 0;
 	protected $id;
@@ -59,38 +62,81 @@ class Chart extends ViewableData {
 		if(! $params) $params = array();
 		$params[self::$type_param] = $this->type;
 		$params[self::$size_param] = ($this->width ? $this->width : self::$default_width) . 'x' . ($this->height ? $this->height : self::$default_height);
-		if($this->title) {
+		
+		// Chart Title : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_chart_title
+		
+		if($this->title && $this->showTitle) {
 			$title = str_replace(' ', '+', $this->title);
 			$params[self::$title_param] = str_replace("\n", '|', $title);
 			if($this->titleColor) {
 				$params[self::$title_style_param] = $this->titleColor;
-				if($this->titleSize) $params[self::$title_style_param] .= ",$this->titleSize";
+				if($this->titleSize) {
+					$params[self::$title_style_param] .= ",$this->titleSize";
+				}
 			}
 		}
-		if($this->marginTop) $params[self::$margin_param] = "$this->marginLeft,$this->marginRight,$this->marginTop,$this->marginBottom";
+		
+		// Chart Margins : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_chart_margins
+		
+		if($this->marginTop) {
+			$params[self::$margin_param] = "$this->marginLeft,$this->marginRight,$this->marginTop,$this->marginBottom";
+		}
+		
+		// Chart Legend Text And Style : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_legend
+		
 		if(array_key_exists(self::$legend_labels_param, $params) && ($this->legendPosition || $this->legendOrder)) {
-			if($this->legendPosition) $params[self::$legend_settings_param] = $this->legendPosition;
-			if($this->legendOrder) {
-				if(array_key_exists(self::$legend_settings_param, $params)) $params[self::$legend_settings_param] .= '|';
-				$params[self::$legend_settings_param] .= is_array($this->legendOrder) ? implode(',', $this->legendOrder) : $this->legendOrder;
+			if($this->legendPosition) {
+				$params[self::$legend_position_param] = $this->legendPosition;
 			}
-			if(array_key_exists(self::$margin_param, $params) && $this->legendWidth) $params[self::$margin_param] .= "|$this->legendWidth,$this->legendHeight";
+			if($this->legendOrder) {
+				if(array_key_exists(self::$legend_position_param, $params)) $params[self::$legend_position_param] .= '|';
+				$params[self::$legend_position_param] .= is_array($this->legendOrder) ? implode(',', $this->legendOrder) : $this->legendOrder;
+			}
+			if(array_key_exists(self::$margin_param, $params) && $this->legendWidth) {
+				$params[self::$margin_param] .= "|$this->legendWidth,$this->legendHeight";
+			}
+			if($this->legendColor && $this->legendFontSize) {
+				$params[self::$legend_style_param] = "$this->legendColor,$this->legendFontSize";
+			}
 		}
+		
+		// Axis Styles & Labels : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_axis_styles_labels
+		
 		if($this->visibleAxes) {
 			$params[self::$visible_axes_param] = implode(',', $this->visibleAxes);
 			if($this->axisRange) {
 				foreach($this->axisRange as $index => $range) {
 					if($range) $ranges[] = "$index," . implode(',', $range);
 				}
-				if(isset($ranges)) $params[self::$axis_range_param] = implode('|', $ranges);
+				if(isset($ranges)) {
+					$params[self::$axis_range_param] = implode('|', $ranges);
+				}
 			}
 			if($this->axisLabels) {
 				foreach($this->axisLabels as $index => $labels) {
 					if($labels) $axesLabels[] = "$index:|" . implode('|', $labels);
 				}
-				if(isset($axesLabels)) $params[self::$axis_labels_param] = implode('', $axesLabels);
+				if(isset($axesLabels)) {
+					$params[self::$axis_labels_param] = implode('', $axesLabels);
+				}
 			}
 		}
+		
+		// Background Fill : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_background_fills
+		
+		if($this->backgroundColor) {
+			$backgroundFills[] = "bg,s,$this->backgroundColor";
+		}
+		if($this->chartColor) {
+			$backgroundFills[] = "c,s,$this->chartColor";
+		}
+		if($this->transparency) {
+			$backgroundFills[] = "a,s,000000$this->transparency";
+		}
+		if(isset($backgroundFills)) {
+			$params[self::$background_fill_param] = implode('|', $backgroundFills);
+		}
+		
 		foreach($params as $name => $value) $paramValues[] = "$name=$value";
 		return self::$base_url . implode('&', $paramValues);
 	}
@@ -109,11 +155,32 @@ class Chart extends ViewableData {
 		$this->generateColor = $value;
 	}
 	
+	static function get_hexa_color() {
+		$hexa = dechex(rand(0, hexdec('FFFFFF')));
+		while(strlen($hexa) < 6) $hexa = "0$hexa";
+		return $hexa;
+	}
+	
+	// Chart Title : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_chart_title
+	
 	function setTitle($title, $color = null, $size = null) {
 		$this->title = $title;
 		if($color) $this->titleColor = $color;
 		if($size) $this->titleSize = $size;
 	}
+	
+	function setShowTitle($show) {$this->showTitle = $show;}
+	
+	// Chart Margins : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_chart_margins
+	
+	function setMargins($top, $right, $bottom, $left) {
+		$this->marginTop = $top;
+		$this->marginRight = $right;
+		$this->marginBottom = $bottom;
+		$this->marginLeft = $left;
+	}
+	
+	// Chart Legend Text And Style : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_legend
 	
 	function setLegendPosition($position) {
 		if(in_array($position, $this->stat('legend_positions'))) $this->legendPosition = $position;
@@ -123,21 +190,23 @@ class Chart extends ViewableData {
 		if(is_array($order) || in_array($order, self::$legend_orders)) $this->legendOrder = $order;
 	}
 	
-	function setMargins($top, $right, $bottom, $left) {
-		$this->marginTop = $top;
-		$this->marginRight = $right;
-		$this->marginBottom = $bottom;
-		$this->marginLeft = $left;
-	}
-	
 	function setLegendSize($width, $height) {
 		$this->legendWidth = $width;
 		$this->legendHeight = $height;
 	}
 	
+	function setLegendFontSize($size) {$this->legendFontSize = $size;}
+	function setLegendColor($color) {$this->legendColor = $color;}
+	
+	function setShowLegend($show) {$this->showLegend = $show;}
+	
+	// Visible Axes : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#axis_type
+	
 	function setVisibleAxes($axes) {
 		if(is_array($axes) && count(array_diff($axes, self::$visible_axes)) == 0) $this->visibleAxes = $axes;
 	}
+	
+	// Axis Range : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#axis_range
 	
 	function setAxesRange($ranges) {
 		if(is_array($ranges)) {
@@ -147,6 +216,8 @@ class Chart extends ViewableData {
 		}
 	}
 	
+	// Custom Axis Labels : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#axis_labels
+	
 	function setAxesLabels($axeslabels) {
 		if(is_array($axeslabels)) {
 			foreach($axeslabels as $index => $labels) {
@@ -155,10 +226,18 @@ class Chart extends ViewableData {
 		}
 	}
 	
-	static function get_hexa_color() {
-		$hexa = dechex(rand(0, hexdec('FFFFFF')));
-		while(strlen($hexa) < 6) $hexa = "0$hexa";
-		return $hexa;
+	// Solid Fills : http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html#gcharts_solid_fills
+	
+	function setBackgroundColor($backgroundColor) {
+		$this->backgroundColor = $backgroundColor;
+	}
+	
+	function setChartColor($chartColor) {
+		$this->chartColor = $chartColor;
+	}
+	
+	function setTransparency($transparency) {
+		$this->transparency = dechex(min(max(0, $transparency), 255));
 	}
 }
 
--- google_charts/code/PieChart.php
+++ google_charts/code/PieChart.php
@@ -3,7 +3,7 @@
 /**
  * This class is used to create a pie chart as an image using google charts
  *
- * @link http://code.google.com/apis/chart/docs/gallery/pie_charts.html
+ * @link http://code.google.com/apis/chart/image/docs/gallery/pie_charts.html
  * @see Chart
  * @package googlecharts
  * @author Romain Louis <romain@sunnysideup.co.nz>
@@ -21,6 +21,7 @@ class PieChart extends Chart {
 
 	protected $colors = array();
 	protected $rotation;
+	protected $showLabels = true;
 
 	function addPortion($portion, $color = null, $legend = null, $label = null) {
 		$portion = array('value' => $portion);
@@ -34,7 +35,7 @@ class PieChart extends Chart {
 		if(count($this->portions) > 0) {
 			foreach($this->portions as $portion) {
 				$portions[] = $portion['value'];
-				if(isset($portion['color'])) $colors[] = $portion['color'];
+				if(isset($portion['color'])) $colors[] = str_replace('#', '', $portion['color']);
 				else if($this->generateColor) $colors[] = Chart::get_hexa_color();
 				if(isset($portion['legend'])) $legend[] = str_replace(' ', '+', $portion['legend']);
 				if(isset($portion['label'])) $label[] = str_replace(' ', '+', $portion['label']);
@@ -47,8 +48,8 @@ class PieChart extends Chart {
 			$params[Chart::$data_param] = 't:' . implode(',', $portions);
 			if(isset($colors)) $params[Chart::$color_param] = implode(count($colors) == count($this->portions) ? '|' : ',', $colors);
 			else if(count($this->colors) > 0) $params[Chart::$color_param] = implode(',', $this->colors);
-			if(isset($legend)) $params[Chart::$legend_labels_param] = implode('|', $legend);
-			if(isset($label)) $params[self::$labels_param] = implode('|', $label);
+			if(isset($legend) && $this->showLegend) $params[Chart::$legend_labels_param] = implode('|', $legend);
+			if(isset($label) && $this->showLabels) $params[self::$labels_param] = implode('|', $label);
 			if($this->rotation) $params[self::$rotation_param] = $this->rotation;
 		}
 		return parent::Link($params);
@@ -60,26 +61,33 @@ class PieChart extends Chart {
 	}
 
 	function setRotation($rotation) {$this->rotation = $rotation;}
+	
+	function setShowLabels($show) {$this->showLabels = $show;}
 }
 
 /**
  * This class is used to create an interactive pie chart using javascript
  *
- * @link http://code.google.com/apis/visualization/documentation/gallery/piechart.html
+ * @link http://code.google.com/apis/chart/interactive/docs/gallery/piechart.html
  * @see PieChart
  * @package googlecharts
  * @author Romain Louis <romain@sunnysideup.co.nz>
  */
 class PieChart_Interactive extends PieChart {
-
-	static $legend_positions = array('left', 'right', 'none');
-
-	protected $backgroundColor;
-	protected $fontSize;
-
-	protected $legendFontSize, $legendColor;
+	
+	static $legend_positions = array('left', 'right', 'top', 'bottom', 'none');
+	static $pie_slice_text_formats = array('percentage', 'value', 'label', 'none');
+	
+	protected $chartBorderColor, $chartBorderWidth;
+	protected $chartDiameter;
+	protected $fontSize, $fontName;
+	protected $is3D;
+	protected $legendFontName;
+	protected $pieSliceText, $pieSliceColor, $pieSliceFontSize, $pieSliceFontName;
 	protected $reverseCategories;
-	protected $tooltipFontSize;
+	protected $sliceVisibilityThreshold, $pieResidueSliceColor, $pieResidueSliceLabel;
+	protected $titleFontName;
+	protected $tooltipColor, $tooltipFontSize, $tooltipFontName;
 	
 	static function addRequirements() {
 		Requirements::javascript('http://www.google.com/jsapi');
@@ -113,24 +121,65 @@ class PieChart_Interactive extends PieChart {
 
 		$options['width'] = $this->width ? $this->width : Chart::$default_width;
 		$options['height'] = $this->height ? $this->height : Chart::$default_height;
-
-		if(isset($colors)) $options['colors'] = $colors;
-
-		if($this->title) $options['title'] = $this->title;
-		if($this->titleColor) $options['titleTextColor'] = $this->titleColor;
-		if($this->titleSize) $options['titleFontSize'] = $this->titleSize;
-
+		
 		if($this->backgroundColor) $options['backgroundColor'] = $this->backgroundColor;
+		if($this->chartBorderColor) $options['backgroundColor.stroke'] = $this->chartBorderColor;
+		if($this->chartBorderWidth) $options['backgroundColor.strokeWidth'] = $this->chartBorderWidth;
+		if($this->chartColor) $options['backgroundColor.fill'] = $this->chartColor;
+		
+		if(isset($colors)) $options['colors'] = $colors;
+		
+		if(isset($this->marginLeft)) $chartAreaOptions['left'] = $this->marginLeft;
+		if(isset($this->marginTop)) $chartAreaOptions['top'] = $this->marginTop;
+		if($this->chartDiameter) $chartAreaOptions['width'] = $chartAreaOptions['height'] = $this->chartDiameter;
+		if(isset($chartAreaOptions)) {
+			$options['chartArea'] = $chartAreaOptions;
+		}
+		
 		if($this->fontSize) $options['fontSize'] = $this->fontSize;
-
+		if($this->fontName) $options['fontName'] = $this->fontName;
+		
+		if($this->is3D) $options['is3D'] = 'true';
+		
 		if($this->legendPosition) $options['legend'] = $this->legendPosition;
-		if($this->legendFontSize) $options['legendFontSize'] = $this->legendFontSize;
-		if($this->legendColor) $options['legendTextColor'] = $this->legendColor;
-
+		if($this->legendColor) $legendTextStyleOptions['color'] = $this->legendColor;
+		if($this->legendFontSize) $legendTextStyleOptions['fontSize'] = $this->legendFontSize;
+		if($this->legendFontName) $legendTextStyleOptions['fontName'] = $this->legendFontName;
+		if(isset($legendTextStyleOptions)) {
+			$options['legendTextStyle'] = $legendTextStyleOptions;
+		}
+		
+		if($this->pieSliceText) $options['pieSliceText'] = $this->pieSliceText;
+		if($this->pieSliceColor) $pieSliceTextStyleOptions['color'] = $this->pieSliceColor;
+		if($this->pieSliceFontSize) $pieSliceTextStyleOptions['fontSize'] = $this->pieSliceFontSize;
+		if($this->pieSliceFontName) $pieSliceTextStyleOptions['fontName'] = $this->pieSliceFontName;
+		if(isset($pieSliceTextStyleOptions)) {
+			$options['pieSliceTextStyle'] = $pieSliceTextStyleOptions;
+		}
+		
 		if($this->reverseCategories) $options['reverseCategories'] = 'true';
-
-		if($this->tooltipFontSize) $options['tooltipFontSize'] = $this->tooltipFontSize;
-
+		
+		if($this->sliceVisibilityThreshold) $options['sliceVisibilityThreshold'] = $this->sliceVisibilityThreshold;
+		if($this->pieResidueSliceColor) $options['pieResidueSliceColor'] = $this->pieResidueSliceColor;
+		if($this->pieResidueSliceLabel) $options['pieResidueSliceLabel'] = $this->pieResidueSliceLabel;
+		
+		if($this->showTitle) {
+			if($this->title) $options['title'] = $this->title;
+			if($this->titleColor) $titleTextStyleOptions['color'] = $this->titleColor;
+			if($this->titleSize) $titleTextStyleOptions['fontSize'] = $this->titleSize;
+			if($this->titleFontName) $titleTextStyleOptions['fontName'] = $this->titleFontName;
+			if(isset($titleTextStyleOptions)) {
+				$options['titleTextStyle'] = $titleTextStyleOptions;
+			}
+		}
+		
+		if($this->tooltipColor) $tooltipTextStyleOptions['color'] = $this->tooltipColor;
+		if($this->tooltipFontSize) $tooltipTextStyleOptions['fontSize'] = $this->tooltipFontSize;
+		if($this->tooltipFontName) $tooltipTextStyleOptions['fontName'] = $this->tooltipFontName;
+		if(isset($tooltipTextStyleOptions)) {
+			$options['tooltipTextStyle'] = $tooltipTextStyleOptions;
+		}
+		
 		$params['options'] = $options;
 		return $params;
 	}
@@ -138,18 +187,52 @@ class PieChart_Interactive extends PieChart {
 	function getJavascript() {
 		$params = $this->getJavascriptParams();
 		$params = Convert::array2json($params);
-		return "draw{$this->class}($params);";
+		return "drawPieChart_Interactive($params);";
 	}
 	
-	function setBackgroundColor($color) {$this->backgroundColor = $color;}
-	function setFontSize($size) {$this->fontSize = $size;}
-
-	function setLegendFontSize($size) {$this->legendFontSize = $size;}
-	function setLegendColor($color) {$this->legendColor = $color;}
-
+	function setChartBorderColor($color) {$this->chartBorderColor = $color;}
+	function setChartBorderWidth($width) {$this->chartBorderWidth = $width;}
+	
+	function setChartArea($left, $top, $diameter) {
+		$this->marginLeft = $left;
+		$this->marginTop = $top;
+		$this->chartDiameter = $diameter;
+	}
+	
+	function setFont($size, $name) {
+		$this->fontSize = $size;
+		$this->fontName = $name;
+	}
+	
+	function enable3D() {$this->is3D = true;}
+	
+	function setLegendFontName($name) {$this->legendFontName = $name;}
+	
+	function setPieSliceText($format) {
+		if(in_array($format, self::$pie_slice_text_formats)) {
+			$this->pieSliceText = $format;
+		}
+	}
+	
+	function setPieSliceTextStyle($color, $fontName, $fontSize) {
+		$this->pieSliceColor = $color;
+		$this->pieSliceFontName = $fontName;
+		$this->pieSliceFontSize = $fontSize;
+	}
+	
 	function reverseCategories() {$this->reverseCategories = true;}
-
-	function setTooltipFontSize($size) {$this->tooltipFontSize = $size;}
+	
+	function setSliceVisibilityThreshold($threshold) {$this->sliceVisibilityThreshold = $threshold;}
+	function setPieResidueSliceColor($color) {$this->pieResidueSliceColor = $color;}
+	function setPieResidueSliceLabel($label) {$this->pieResidueSliceLabel = $label;}
+	
+	function setTitleFontName($name) {$this->titleFontName = $name;}
+	
+	function setTooltipTextStyle($color, $fontName, $fontSize) {
+		$this->tooltipColor = $color;
+		$this->tooltipFontName = $fontName;
+		$this->tooltipFontSize = $fontSize;
+	}
 }
 
 ?>
