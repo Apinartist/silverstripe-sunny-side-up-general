107ce5c193c48ae20f8ca132f6346f9de63aadf1
Nicolaas <github@sunnysideup.co.nz>
2011-07-15 08:56:41 +0000

    improving mysite/_config.php

--- ecommerce_test/mysite/_config.php
+++ ecommerce_test/mysite/_config.php
@@ -208,13 +208,13 @@ ProductAttributeDecoratorColour_Value::set_default_colour("000000");
 
 
 //===================---------------- START ecommerce_Repeatorders MODULE ----------------===================
-Object::add_extension('Order', 'AutomaticallyCreatedOrderDecorator');
-RepeatOrder::set_delivery_days(array('Tuesday','Wednesday','Thursday','Friday'));
-RepeatOrder::set_payment_methods(array(
-	'DirectCreditPayment'  => 'Direct Credit (payment into bank account)',
-	'DpsPxPayStoredPayment'  => 'Saved Credit Card using DPS'
-));
-RepeatOrder::set_minimum_days_in_the_future(3);
+//Object::add_extension('Order', 'AutomaticallyCreatedOrderDecorator');
+//RepeatOrder::set_delivery_days(array('Tuesday','Wednesday','Thursday','Friday'));
+//RepeatOrder::set_payment_methods(array(
+	//'DirectCreditPayment'  => 'Direct Credit (payment into bank account)',
+	//'DpsPxPayStoredPayment'  => 'Saved Credit Card using DPS'
+//));
+//RepeatOrder::set_minimum_days_in_the_future(3);
 //===================---------------- END ecommerce_Repeatorders MODULE ----------------===================
 
 
