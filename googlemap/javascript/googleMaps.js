38f3cc2c0ec94c12734cba24d08988ced1dbcc38
Nicolaas <github@sunnysideup.co.nz>
2011-07-19 17:21:08 +0000

    fix in JS

--- googlemap/javascript/googleMaps.js
+++ googlemap/javascript/googleMaps.js
@@ -866,7 +866,7 @@ GMC.prototype.createSideBar = function(sideBarArray) {
 				i = sideBarElements[1];
 				layerName = this.gmarkers[i].layerId;				
 				if(!strpos(this.gmarkers[i].serverId, "manuallyAdded", 0)) {
-					html += '<li class="forLayer'+layerName+'"><a href="'+ this.currentPageURL + '#map" onclick="GMO.showMarkerFromList(' + i + '); return false;">' + this.gmarkers[i].markerName + '</a> <div class="infowindowDetails">'  + this.gmarkers[i].markerDesc + '</div></li>';
+					html += '<li class="forLayer'+layerName+' icon'+i+'"><a href="'+ this.currentPageURL + '#map" onclick="GMO.showMarkerFromList(' + i + '); return false;">' + this.gmarkers[i].markerName + '</a> <div class="infowindowDetails">'  + this.gmarkers[i].markerDesc + '</div></li>';
 				}
 				else {
 					html += '<li class="forLayer'+layerName+'">You added: <a href="'+ this.currentPageURL + '#map" onclick="GMO.showMarkerFromList(' + i + '); return false;">' + this.gmarkers[i].markerName + '</a></li>';
