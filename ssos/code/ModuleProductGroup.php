5cbd4cc351191185a90abb981830cfe65b26c5cb
Nicolaas <github@sunnysideup.co.nz>
2011-08-10 23:48:44 +0000

    adding Module Product

---
+++ ssos/code/ModuleProductGroup.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ *
+ *
+ *
+ **/ 
+
+
+class ModuleProductGroup extends ProductGroup {
+
+	public static $hide_ancester = "ProductGroup";
+
+}
+
+
+class ModuleProduct_Controller extends Product_Controller {
+
+
+}
