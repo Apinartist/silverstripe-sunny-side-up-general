32ccb8206f8e2d17b61e03275cbcfdd16cf511de
Nicolaas <github@sunnysideup.co.nz>
2011-08-12 03:29:23 +0000

    Documented WordCloudChart_Rotating

--- google_charts/code/WordCloudChart.php
+++ google_charts/code/WordCloudChart.php
@@ -5,13 +5,21 @@
  * 
  * @link http://visapi-gadgets.googlecode.com/svn/trunk/termcloud/doc.html
  * @see Chart
+ * @todo Currently links all words to "".  Need to implement passing in URLs or see if there's a way to turn off empty links
  * @package googlecharts
  * @author Romain Louis <romain@sunnysideup.co.nz>
  */
 class WordCloudChart extends Chart {
-	
+	/**
+	 * The array of words to display on the chart
+	 * 
+	 * @var array
+	 */
 	protected $words;
 
+	/**
+	 * Includes the needed JS files via the Requirements system
+	 */
 	static function addRequirements() {
 		Requirements::css('http://visapi-gadgets.googlecode.com/svn/trunk/termcloud/tc.css');
 		Requirements::javascript('http://visapi-gadgets.googlecode.com/svn/trunk/termcloud/tc.js');
@@ -19,21 +27,37 @@ class WordCloudChart extends Chart {
 		Requirements::javascript('googlecharts/javascript/wordcloud.js');
 	}
 	
+	/**
+	 * Takes an array of words, with the word as the key and the frequency as the value.
+	 * Font sizes and colours are determined by relative frequencies.
+	 * 
+	 * @param array $words An array of words to displayin the format array('myword' => 12);
+	 */
 	public function __construct(array $words) {
 		parent::__construct();
 		$this->words = $words;
 		self::addRequirements();
 	}
 	
+	/**
+	 * Standard SS method
+	 * 
+	 * @return string A <div> tag and the javascript needed to convert it to a graph
+	 */
 	public function forTemplate() {
 		$params = $this->getJavascriptParams();
-		$width = $this->width ? $this->width : Chart::$default_width;
+		$width = ($this->width ? $this->width : Chart::$default_width);
 		
 		$script = $this->getJavascript();
 		Requirements::customScript("google.setOnLoadCallback(function() {{$script}});");
 		return "<div id=\"{$params['id']}\" class=\"wordcloud\" style=\"width: {$width}px;\"></div>";
 	}
 	
+	/**
+	 * Returns an array of parameters that will be passed to the JS chart.draw() function
+	 * 
+	 * @return array An associative array of parameters
+	 */
 	public function getJavascriptParams() {
 		return array(
 			'id' => "WC_$this->id",
@@ -42,6 +66,11 @@ class WordCloudChart extends Chart {
 		);
 	}
 	
+	/**
+	 * Returns the JS function call that generates the chart in string format
+	 * 
+	 * @return string
+	 */
 	public function getJavascript() {
 		$params = $this->getJavascriptParams();
 		$params = Convert::array2json($params);
@@ -59,13 +88,40 @@ class WordCloudChart extends Chart {
  */
 class WordCloudChart_Rotating extends Chart {
 	
-	public static $font_max = 30;
-	public static $font_min = 7;	
+	/**
+	 * The minimum font size for the cloud
+	 * 
+	 * @var int
+	 */
+	public static $font_min = 7;
 	
-	protected $words, $urls;
+	/**
+	 * The maximum font size for the cloud
+	 * 
+	 * @var int
+	 */
+	public static $font_max = 30;
+	
+	/**
+	 * The array of words to display on the chart
+	 * 
+	 * @var array
+	 */
+	protected $words;
+	
+	/**
+	 * The array of URLs that the correspondings words in $words will link to
+	 * 
+	 * @var array
+	 */
+	protected $urls;
+	
+	
 	protected $speed, $color, $hoverColor;
 	
-	
+	/**
+	 * Includes the needed JS files via the Requirements system
+	 */
 	static function addRequirements() {
 		Requirements::javascript('http://www.google.com/jsapi');
 		Requirements::javascript('http://word-cumulus-goog-vis.googlecode.com/svn/trunk/wordcumulus.js');
@@ -73,21 +129,40 @@ class WordCloudChart_Rotating extends Chart {
 		Requirements::javascript('googlecharts/javascript/wordcloudrotating.js');
 	}
 	
+	/**
+	 * Takes an array of words, with the word as the key and the frequency as the value.
+	 * Font sizes and colours are determined by relative frequencies.
+	 * 
+	 * @param array $words An array of words to displayin the format array('myword' => 12);
+	 */
 	public function __construct(array $words) {
 		parent::__construct();
 		$max = max($words);
 		$min = min($words);
+		
 		$aFactor = (self::$font_max - self::$font_min) / ($max - $min);
 		$bFactor = self::$font_max - $max * $aFactor;
-		foreach($words as $word => $total) $words[$word] = round($total * $aFactor + $bFactor, 1);
+		foreach($words as $word => $total) {
+			$words[$word] = round($total * $aFactor + $bFactor, 1);
+		}
 		$this->words = $words;
 		self::addRequirements();
 	}
 	
+	/**
+	 * Set the URLs that each word in the cloud will link to.
+	 * 
+	 * @param array $words An array of words to display in the format array('myword' => 'http://www.example.com');
+	 */
 	public function setURLS($urls) {
 		$this->urls = $urls;
 	}
 	
+	/**
+	 * Standard SS method
+	 * 
+	 * @return string A <div> tag and the javascript needed to convert it to a graph
+	 */
 	public function forTemplate() {
 		$params = $this->getJavascriptParams();
 		
@@ -96,6 +171,11 @@ class WordCloudChart_Rotating extends Chart {
 		return "<div id=\"{$params['id']}\" class=\"wordcloudrotating\"></div>";
 	}
 	
+	/**
+	 * Returns an array of parameters that will be passed to the JS chart.draw() function
+	 * 
+	 * @return array An associative array of parameters
+	 */
 	public function getJavascriptParams() {
 		$params = array(
 			'id' => "WCR_$this->id",
@@ -114,31 +194,60 @@ class WordCloudChart_Rotating extends Chart {
 		}
 		$params['urls'] = $urls;
 		
-		$options['width'] = $this->width ? $this->width : Chart::$default_width;
-		$options['height'] = $this->height ? $this->height : Chart::$default_height;
+		$options['width'] = ($this->width ? $this->width : Chart::$default_width);
+		$options['height'] = ($this->height ? $this->height : Chart::$default_height);
 		
-		if($this->speed) $options['speed'] = $this->speed;
-		if($this->color) $options['text_color'] = $this->color;
-		if($this->hoverColor) $options['hover_text_color'] = $this->hoverColor;
+		if($this->speed) {
+			$options['speed'] = $this->speed;
+		}
+		
+		if($this->color) {
+			$options['text_color'] = $this->color;
+		}
+		
+		if($this->hoverColor) {
+			$options['hover_text_color'] = $this->hoverColor;
+		}
 		
 		$params['options'] = $options;
 		return $params;
 	}
 	
+	/**
+	 * Returns the JS function call that generates the chart in string format
+	 * 
+	 * @return string
+	 */
 	public function getJavascript() {
 		$params = $this->getJavascriptParams();
 		$params = Convert::array2json($params);
 		return "drawWordCloudChart_Rotating($params);";
 	}
 	
+	/**
+	 * Set the speed the cloud rotates at.  Default is 100, 
+	 * 
+	 * @param int
+	 */
 	public function setSpeed($speed) {
 		$this->speed = $speed;
 	}
 	
+	/**
+	 * Set the text colour in #RRGGBB format
+	 * 
+	 * @param string
+	 */
 	public function setColor($color) {
 		$this->color = $color;
 	}
 	
+	/**
+	 * Set the hover text colour in #RRGGBB format
+	 * NOTE: this currently does not work {@link http://code.google.com/p/word-cumulus-goog-vis/issues/detail?id=2}
+	 * 
+	 * @param string
+	 */
 	public function setHoverColor($hoverColor) {
 		$this->hoverColor = $hoverColor;
 	}
