0bed657e01a06f9326d0f388ff647ab9ee16c843
Nicolaas <github@sunnysideup.co.nz>
2011-08-13 01:10:11 +0000

    adding background image

--- ecommerce_test/mysite/code/Page.php
+++ ecommerce_test/mysite/code/Page.php
@@ -1,11 +1,29 @@
 <?php
+
 class Page extends SiteTree {
 
 	public static $db = array(
 	);
 
 	public static $has_one = array(
+		"BackgroundImage" => "Image"	
 	);
+
+	function MyBackgroundImage() {
+		if($this->BackgroundImageID) {
+			if($image = $this->BackgroundImage()) {
+				return $image;
+			}
+		}
+		if($this->ParentID) {
+			if($parent = DataObject::get_by_id("SiteTree", "ParentID = ".$this->ParentID)) {
+				return $parent->MyBackgroundImage();
+			}
+		}
+		if($siteConfig = SiteConfig::current_site_config()) {
+			return $siteConfig->MyBackgroundImage();
+		}
+	}	
 
 }
 class Page_Controller extends ContentController {
