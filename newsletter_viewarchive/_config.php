dac438bc4f252d953f14222afb431f24f23f2847
Nicolaas <github@sunnysideup.co.nz>
2011-09-10 08:48:21 +0000

    adding decorator

--- newsletter_viewarchive/_config.php
+++ newsletter_viewarchive/_config.php
@@ -8,4 +8,5 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START newsletter_viewarchive MODULE ----------------===================
+//Object::add_extension('Newsletter', 'NewsletterArchiveDecorator');
 //=====================---------------- END newsletter_viewarchive MODULE ----------------===================
