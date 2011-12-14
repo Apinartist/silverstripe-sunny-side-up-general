a53f831883bddaa67f7cb24212a8a4dcb8e4d082
Nicolaas <github@sunnysideup.co.nz>
2011-12-14 07:18:48 +0000

    MINOR: cleanup of config file

--- menucache/_config.php
+++ menucache/_config.php
@@ -5,16 +5,18 @@
  **/
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START metatags MODULE ----------------===================
+//===================---------------- START menucache MODULE ----------------===================
+//MENU CHACHING
 //Object::add_extension('SiteTree', 'MenuCache');
 //Object::add_extension('ContentController', 'MenuCache_Controller');
 //MenuCache::set_fields(array("Header", "Menu", "Footer", "LayoutSection"));
 //MenuCache::set_layout_field(3); //NOTA BENE - MAKE SURE TO SET TO ANYTHING BUT 0-4 IF YOU DO NOT INCLUDE A LAYOUT SECTION!
 
-//-- static publishing --
+//STATIC PUBLISHING
 //Object::add_extension('SiteTree', 'MenuCacheStatic');
 //Object::add_extension("SiteTree", "FilesystemPublisher('cache/', 'html')");
 //MenuCacheStatic::add_class_name_to_cache("SiteTree");
+//OPTIONAL SETTINGS
 //MenuCacheStatic::remove_class_name_from_cache("UserDefinedForm");
 //MenuCacheStatic::add_custom_url_to_cache("SiteTree");
 //StaticPublisher::$disable_realtime = false;
@@ -22,5 +24,5 @@
 //StaticPublisher::set_static_publisher_theme(false);
 //FilesystemPublisher::set_static_base_url()
 //FilesystemPublisher::set_static_publisher_theme()
-//===================---------------- START metatags MODULE ----------------===================
+//===================---------------- END menucache MODULE ----------------===================
 
