9d99519a949612b1d40fd9b78801ab480ee6812c
Nicolaas <github@sunnysideup.co.nz>
2011-12-14 07:27:51 +0000

    MINOR: cleanup of config file

--- dataintegritytests/_config.php
+++ dataintegritytests/_config.php
@@ -7,13 +7,12 @@
  * @help: // to see in action, add _configs per below, and go to http://www.mysite.com/dbintegritycheck/
  **/
 
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START dataintegritytests MODULE ----------------===================
-
 Director::addRules(7, array(
 	'dbintegritycheck//$Action/$ID/$OtherID' => 'DataIntegrityTest'
 ));
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START dataintegritytests MODULE ----------------===================
 /*
 DataIntegrityTest::set_fields_to_delete(
 	array(
