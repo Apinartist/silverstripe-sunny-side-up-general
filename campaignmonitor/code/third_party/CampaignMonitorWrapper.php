2c0ff79bd16bd7ec9af756599be510d264147ccd
Nicolaas <github@sunnysideup.co.nz>
2012-03-29 02:48:56 +0000

    get sent campaigns method

--- campaignmonitor/code/third_party/CampaignMonitorWrapper.php
+++ campaignmonitor/code/third_party/CampaignMonitorWrapper.php
@@ -172,10 +172,6 @@ class CampaignMonitorWrapper extends Object {
 		}
 	}
 	
-	public function clientGetSentCampaigns() {
-		return file_get_contents('http://api.createsend.com/api/v3/clients/' . self::$client_ID . '/campaigns.xml');
-	}
-	
 	public function clientGetDetail() {
 		return self::$cm->clientGetDetail( self::$client_ID );
 	}
