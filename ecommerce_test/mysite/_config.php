1b2052975fd3c241e5ddda6f60c1b3aef8028a3a
Nicolaas <github@sunnysideup.co.nz>
2012-04-11 21:20:49 +0000

    ENHANCEMENT: adding better externals for MetaTags

--- ecommerce_test/mysite/_config.php
+++ ecommerce_test/mysite/_config.php
@@ -72,7 +72,32 @@ Payment::set_supported_methods(array(
 // __________________________________ END PAYMENT MODULE CONFIG __________________________________
 
 
-
+//===================---------------- START metatags MODULE ----------------===================
+// dont forget to add $this->addBasicMetatagRequirements() to Page_Controller->init();
+// and add this to your Page.ss template file: $ExtendedMetatags
+//MUST SET ...
+Object::add_extension('SiteConfig', 'MetaTagSiteConfigExtension');
+Object::add_extension('SiteTree', 'MetaTagAutomation');
+Object::add_extension('ContentController', 'MetaTagAutomation_controller');
+//MAY SET ...
+/* pop-ups and form interaction */
+//MetaTagAutomation::set_disable_update_popup(false);
+//MetaTagAutomation::set_meta_data_coding("your name goes here");
+//MetaTagAutomation::set_meta_data_design("your designer's name goes here");
+/* meta descriptions */
+//MetaTagAutomation::set_meta_desc_length(24);
+/* meta keywords */
+//MetaTagAutomation::set_hide_keywords_altogether(true);
+//FONTS - see google fonts for options, include within CSS file as: body {font-family: Inconsolata;}
+MetaTagAutomation::add_google_font("Voces");
+MetaTagAutomation::add_google_font("Quattrocento+Sans");
+/* combined files */
+MetaTagAutomation_controller::set_folder_for_combined_files("cache");
+MetaTagAutomation_controller::set_combine_css_files_into_one(true);
+MetaTagAutomation_controller::set_combine_js_files_into_one(true);
+/* favicons */
+//MetaTagAutomation::set_use_themed_favicon(true);
+//===================---------------- END metatags MODULE ----------------===================
 
 //===================---------------- START metatags MODULE ----------------===================
 Object::add_extension('SiteTree', 'MetaTagAutomation');
