c00c2058008584577bec03d50708b8d6bbfb9a15
Nicolaas <github@sunnysideup.co.nz>
2012-05-20 02:00:05 +0000

    BUGFIX: improving populateDefaults

--- templateoverview/code/dataobjects/TemplateOverviewTestItem.php
+++ templateoverview/code/dataobjects/TemplateOverviewTestItem.php
@@ -78,6 +78,13 @@ class TemplateOverviewTestItem extends DataObject {
 
 	public function populateDefaults() {
 		parent::populateDefaults();
+		if(isset(self::$defaults)) {
+			foreach(self::$defaults as $fieldName => $fieldValue) {
+				if(!isset($this->$fieldName) || $this->$fieldName === null) {
+					$this->$fieldName = $fieldValue;
+				}
+			}
+		}
 		$this->Sort = 100;
 	}
 
