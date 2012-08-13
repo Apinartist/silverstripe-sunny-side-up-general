d86e1f16c292491e15f300f0d5b9e050ca2664fa
Nicolaas <github@sunnysideup.co.nz>
2012-08-13 23:36:48 +0000

    trimming title

--- metatags/code/MetaTagAutomation.php
+++ metatags/code/MetaTagAutomation.php
@@ -327,7 +327,7 @@ class MetaTagAutomation_controller extends Extension {
 		}
 		if($includeTitle) {
 			$titleTag = '
-			<title>'.Convert::raw2att($siteConfig->PrependToMetaTitle.' '.$title.' '.$siteConfig->AppendToMetaTitle).'</title>';
+			<title>'.trim(Convert::raw2att($siteConfig->PrependToMetaTitle.' '.$title.' '.$siteConfig->AppendToMetaTitle)).'</title>';
 		}
 		else {
 			$titleTag = '';
